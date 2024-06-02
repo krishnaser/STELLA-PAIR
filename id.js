41ede983be7fe0b20d58bfaed6638cb8073f7459
KRISHNA <121825759+krishnaser@users.noreply.github.com>
2024-06-02 16:30:52 +0530

    Update id.js

--- id.js
+++ id.js
@@ -1,6 +1,6 @@
-function makeid(num = 4) {
+function makeid(num = 2) {
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let characters = "KEISHNA963389";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
